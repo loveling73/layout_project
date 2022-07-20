e83b3ebac9ae1ca5ec86627a5662c2dc759aa976
manhpdhe151290 <manhpdhe151290@fpt.edu.vn>
2022-07-20 23:01:07 +0700

    Header component done

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,12 +1,31 @@
 import React from 'react';
+import { MenuFoldOutlined, BellOutlined, DownOutlined } from '@ant-design/icons';
+import styles from './Header.module.scss'
 
 
-function Header({title , icon, avatar ,}) {
+function Header({ title, icon, avatar, }) {
     return (
-        <div>
 
-        </div>
+        <header className={styles.header}>
+            <div className={styles.title}>
+                <div><MenuFoldOutlined style={{ fontSize: '25px' }} /></div>
+                <div style={{ marginLeft: '15px', fontSize: '25px', fontWeight: '650' }}>{title}</div>
+            </div>
+            <nav className={styles.nav}>
+                <div className={styles.parent}>
+                    <span className={styles.dot}></span>
+                    <BellOutlined style={{ fontSize: '25px', marginRight: '30px' }} className="mx-5 my-3 " />
+                </div>
+                <div className={styles.user}>
+                    <img className={styles.avatar} alt='avatar' src={avatar} />
+                    <div className={styles.info}>
+                        <span style={{ fontWeight: '600' }}>Phan Đức Mạnh</span>
+                        <span style={{ fontWeight: '300' }}>Quản trị viên</span>
+                    </div>
+                    <DownOutlined className={styles.downIcon} />
+                </div>
+            </nav>
+        </header>
     );
 }
-
 export default Header;
--- src/screens/Products/Product.js
+++ src/screens/Products/Product.js
@@ -3,13 +3,14 @@ import { Image, Input, Layout, Table } from 'antd';
 import React, { useState } from 'react';
 import avatar from '../../assets/image/avatar.jpg';
 import Button from '../../components/Button/Button';
+import Header from '../../components/Header/Header';
 import Modals from '../../components/Modals';
 import SearchBox from '../../components/SearchBox/SearchBox';
 import SideBar from '../../components/SideBar/SideBar';
 import { arrayitems, columns, data } from '../../utils/fakeDataM';
 import "./index.css";
 
-const { Header, Content, Footer } = Layout;
+const { Content, Footer } = Layout;
 
 const Product = () => {
     const [showModal, setShowModal] = useState(false);
@@ -23,34 +24,7 @@ const Product = () => {
                 <SideBar array={arrayitems} />
                 <Layout className='flex flex-1'>
 
-                    <Header className="site-layout-sub-header-background display" style={{ padding: 0, height: 130 }}>
-                        <div className="flex flex-row justify-between">
-                            <div className="basis-1/4 mx-4 my-3 flex align-center items-center">
-
-                                <MenuFoldOutlined style={{ fontSize: '25px' }} className="mx-2 my-3" />
-                                <span className="mx-5" style={{ fontSize: '20px', fontWeight: 600 }}>Danh sách sản phẩm</span>
-                            </div>
-
-                            <div className="basis-1/4 mx-4 my-3 flex items-center content-end "  >
-                                <BellOutlined style={{ fontSize: '25px', marginRight: '30px' }} className="mx-5 my-3 " />
-                                <Image
-                                    className='rounded-full'
-                                    preview={{ visible: false }}
-                                    width={40}
-                                    src={avatar}
-
-                                />
-                                {showModal && <Modals title={'Tạo kho mới '} input={['Mã kho', 'Tên kho']}></Modals>}
-                                <span className="grid grid-cols-1 mx-5">
-                                    <span style={{ fontSize: '13px' }}>Phan Đức Mạnh</span>
-                                    <span style={{ fontSize: '10px' }}>Quản trị viên</span>
-
-                                </span>
-                                <span><DownOutlined /></span>
-                            </div>
-
-                        </div>
-                    </Header>
+                    <Header title='Danh sách sản phẩm' avatar={avatar} />
                     <Content className="flex align-center items-centent " style={{ padding: 0, height: 40, marginTop: '15px', marginBottom: '50px' }}>
 
 
