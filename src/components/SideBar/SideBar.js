5cc9ddc97231f516cf99388fa76fa6c7f33f3cca
manhpdhe151290 <manhpdhe151290@fpt.edu.vn>
2022-07-22 17:14:10 +0700

    update products screens

--- src/components/SideBar/SideBar.js
+++ src/components/SideBar/SideBar.js
@@ -3,13 +3,14 @@ import { ShopOutlined, BarChartOutlined, RestOutlined, UserOutlined, RiseOutline
 import React, { useState } from 'react';
 import { AiOutlineDown } from 'react-icons/ai'
 import { BsDot } from 'react-icons/bs'
-// import './SiderBar.css';
+import './SideBar.css';
+import avatar from '../../assets/image/avatar.jpg'
 
 
 const { Sider } = Layout;
 
 const Menus = [
-    {   
+    {
         tilte: 'Tổng quan',
         icon: BarChartOutlined
     },
@@ -70,59 +71,59 @@ const Menus = [
 ]
 
 
-function SideBar(props) {
+function SideBar() {
     const [submenuOpen, setSubmenuOpen] = useState(false);
     return (
         <>
-        <div className="bg-dark-purple h-screen p-5 pt-8 w-72" >
-            <Row>
-                <Col span={1}>
-                    <Image className='rounded-full mt-3'
-                        preview={{ visible: false }}
-                        width={40}
-                        src="https://scontent.fhan3-1.fna.fbcdn.net/v/t39.30808-1/282647232_3372145673013688_1051696226724793886_n.jpg?stp=dst-jpg_p240x240&_nc_cat=102&ccb=1-7&_nc_sid=7206a8&_nc_ohc=afQt19GgwJ4AX_-egDC&_nc_ht=scontent.fhan3-1.fna&oh=00_AT_vN3BSoIL8KgPZL6enPTcEqgRkH2FsEo7hkL6Bps-YgA&oe=62D5D37C"
+            <div className="bg-dark-purple commom1 p-5 pt-8 w-72" >
+                <Row>
+                    <Col span={1}>
+                        <Image className='rounded-full mt-3'
+                            preview={{ visible: false }}
+                            width={40}
+                            src={avatar}
 
-                    />
-                </Col>
-                <Col span={12} push={6}>
-                    <h1 className="text-stone-300 pt-5"> Đức Mạnh </h1>
+                        />
+                    </Col>
+                    <Col span={12} push={6}>
+                        <h1 className="text-stone-300 pt-5"> Đức Mạnh </h1>
 
-                </Col>
-            </Row>
-            <hr className="-mx-5 mt-8"></hr>
-            <ul className="pt-2">
-                {Menus.map((menu, index) => (
-                    <>
-                        <li key={index} className="relative text-gray-300 text-md flex item-center cursor-pointer gap-x-4 p-2 hover:bg-blue-white rounded-md mt-2">
-                            <span className="block float-left"> {React.createElement(menu.icon)} </span>
-                            <span className="mt-1 font-medium "> {menu.tilte} </span>
+                    </Col>
+                </Row>
+                <hr className="-mx-5 mt-8"></hr>
+                <ul className="pt-2">
+                    {Menus.map((menu, index) => (
+                        <>
+                            <li key={index} className="relative text-gray-300 text-md flex item-center cursor-pointer gap-x-4 p-2 hover:bg-blue-white rounded-md mt-2">
+                                <span className="block float-left"> {React.createElement(menu.icon)} </span>
+                                <span className="mt-1 font-medium "> {menu.tilte} </span>
 
-                            {menu.submenu && (
-                                <div className="absolute inset-y-5 right-2">
-                                    <AiOutlineDown className={`${submenuOpen && "rotate-180"}`} onClick={() =>
-                                        setSubmenuOpen(!submenuOpen)} />
-                                </div>
-                            )}
-                        </li>
-                        {menu.submenu && submenuOpen && (
-                            <ul>
-                                {menu.submenuItems.map((submenuItems, index) => (
-                                    <div className="">
-                                        <li key={index} className="text-gray-300 text-base flex item-center cursor-pointer gap-x-4 p-2 hover:bg-blue-white rounded-md ml-2">
-                                            <span> <BsDot /> </span>
-                                            {submenuItems.tilte}
-                                        </li>
+                                {menu.submenu && (
+                                    <div className="absolute inset-y-5 right-2">
+                                        <AiOutlineDown className={`${submenuOpen && "rotate-180"}`} onClick={() =>
+                                            setSubmenuOpen(!submenuOpen)} />
                                     </div>
-                                ))},
-                            </ul>
-                        )}
-                    </>
-                ))}
-
-            </ul>
-
-        </div>
+                                )}
+                            </li>
+                            {menu.submenu && submenuOpen && (
+                                <ul>
+                                    {menu.submenuItems.map((submenuItems, index) => (
+                                        <div className="">
+                                            <li key={index} className="text-gray-300 text-base flex item-center cursor-pointer gap-x-4 p-2 hover:bg-blue-white rounded-md ml-2">
+                                                <span > <BsDot></BsDot> </span>
+                                                {submenuItems.tilte}
+                                            </li>
+                                        </div>
+                                    ))}
+                                </ul>
+                            )}
                         </>
+                    ))}
+
+                </ul>
+
+            </div>
+        </>
     );
 }
 
